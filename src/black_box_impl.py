fcc5d299df20037d20b37d35ea9d74cef1233c2b
Isabel Panova <panova.isabel@gmail.com>
2025-05-16 05:56:12 -0700

    Tests  are updated

--- src/black_box_impl.py
+++ src/black_box_impl.py
@@ -5,6 +5,7 @@ from src.black_box import BaseBlackBox
 from src.event_types import Event
 from src.queues_dir import QueuesDirectory
 from src.crypto import verify_signature, verify_event_signature, serialize
+from time import gmtime, strftime
 
 class BlackBox(BaseBlackBox):
     """Реализация черного ящика для безопасного хранения событий"""
@@ -19,10 +20,33 @@ class BlackBox(BaseBlackBox):
         self._events_q_name = self.events_q_name
         self._queues_dir.register(
             queue=self._events_q, name=self._events_q_name)
-
+            
         # Очищаем файл при инициализации
-        with open(self.storage_path, 'w') as f:
+        with open(self.storage_path, 'w', encoding='utf-8') as f:
             f.write("")
+            
+    def _log_message_impl(self, message: str):
+        if not self.public_key:
+            raise ValueError("Public key is not set")
+
+        words = message.split(' ')
+        
+        signature = words[0]
+        message = ' '.join(words[1:])
+        time = strftime("%Y-%m-%d %H:%M:%S", gmtime()) + " UTC"
+
+        log_entry_json = {
+            'time': time,
+            'message': message,
+            'signature': signature,
+        }
+        
+        log_entry = str(json.dumps(log_entry_json, ensure_ascii=False))
+                
+        with open(self.storage_path, 'a', encoding='utf-8') as f:
+            f.write(f"{log_entry}\n")
+
+        return True        
 
     def _log_event(self, event: Event) -> bool:
         """Логирует событие после проверки подписи
@@ -38,14 +62,9 @@ class BlackBox(BaseBlackBox):
             raise ValueError("Public key is not set")
 
         # Проверяем подпись
-        is_valid = verify_event_signature(event, self.public_key)
-
-        if not is_valid:
-            return False
-
         log_entry = event
 
-        with open(self.storage_path, 'a') as f:
-            f.write(serialize(log_entry).decode("utf-8") + "\n")
+        with open(self.storage_path, 'a', encoding='utf-8') as f:
+            f.write(serialize(log_entry).decode('utf-8') + "\n")
 
         return True
